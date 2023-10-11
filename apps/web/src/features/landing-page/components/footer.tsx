12bc29321352f177ff536365941655a487a5e89e
xeuxdev <tochukwujohn24@gmail.com>
2023-10-11 20:41:42 +0100

    add footer

---
+++ apps/web/src/features/landing-page/components/footer.tsx
@@ -0,0 +1,33 @@
+import { Icons } from "@/components/icons"
+import { siteConfig } from "@/config/site"
+import { Github, Send } from "lucide-react"
+import Link from "next/link"
+import React from "react"
+
+function Footer() {
+  return (
+    <footer className="w-full h-12 flex items-center justify-between px-5">
+      <div className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-4">
+          <p className="text-lg font-bold text-primary md:text-xl ">
+            {siteConfig.name}
+          </p>
+
+          <Send className="text-[#BA1B1B]" strokeWidth={3} />
+        </Link>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <Link href={"https://github.com/xeuxdev/easy-mailer"} target="_blank">
+          <Github className="w-7" />
+        </Link>
+
+        <Link href={"https://x.com/xeuxdev"} target="_blank">
+          <Icons.X className="fill-foreground w-7" />
+        </Link>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
