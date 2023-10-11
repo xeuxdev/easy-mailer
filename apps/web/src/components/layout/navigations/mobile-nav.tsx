383f5d2f4168f2a1266707c42c006fb8e1e7b8be
xeuxdev <tochukwujohn24@gmail.com>
2023-10-11 21:57:58 +0100

    update nav for doc sections

--- apps/web/src/components/layout/navigations/mobile-nav.tsx
+++ apps/web/src/components/layout/navigations/mobile-nav.tsx
@@ -13,9 +13,13 @@ import { siteConfig } from "@/config/site"
 import Link from "next/link"
 import { Button } from "../../ui/button"
 import { Icons } from "@/components/icons"
+import { usePathname } from "next/navigation"
 
 function MobileNav() {
   const [isOpen, setIsOpen] = useState(false)
+  const pathname = usePathname()
+
+  const isInDocsPage = pathname.includes("docs")
 
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
@@ -33,23 +37,48 @@ function MobileNav() {
           </SheetTitle>
         </SheetHeader>
 
-        <div className="flex flex-col gap-10 mt-20">
-          {siteConfig.navigations.map((item) => (
-            <Button
-              key={item.name}
-              asChild
-              variant={"link"}
-              className="pl-0 text-left w-fit"
-              onClick={() => {
-                setIsOpen(false)
-              }}
-            >
-              <Link href={item.href} className="uppercase ">
-                {item.name}
-              </Link>
-            </Button>
-          ))}
-        </div>
+        {isInDocsPage ? (
+          <div className="flex flex-col gap-5 py-20">
+            {siteConfig.sidebar.map((item) => {
+              const isActive = pathname.includes(item.href)
+              console.log(pathname)
+              return (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={`capitalize ${
+                    isActive
+                      ? "font-semibold"
+                      : "text-muted-foreground font-medium"
+                  }`}
+                  onClick={() => {
+                    setIsOpen(false)
+                  }}
+                >
+                  {item.name}
+                </Link>
+              )
+            })}
+          </div>
+        ) : (
+          <div className="flex flex-col gap-10 mt-20">
+            {siteConfig.navigations.map((item) => (
+              <Button
+                key={item.name}
+                asChild
+                variant={"link"}
+                className="pl-0 text-left w-fit"
+                onClick={() => {
+                  setIsOpen(false)
+                }}
+              >
+                <Link href={item.href} className="uppercase ">
+                  {item.name}
+                </Link>
+              </Button>
+            ))}
+          </div>
+        )}
       </SheetContent>
     </Sheet>
   )
--- apps/web/src/features/docs/components/side-bar.tsx
+++ apps/web/src/features/docs/components/side-bar.tsx
@@ -1,14 +1,36 @@
 "use client"
+import { siteConfig } from "@/config/site"
 import { useMediaQuery } from "@/hooks/use-media-query"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 import React from "react"
 
 function DocsSideBar() {
   const matches = useMediaQuery("(min-width: 768px)")
+  const pathname = usePathname()
 
   if (!matches) {
     return null
   }
-  return <aside className="col-span-3 ">DocsSideBar</aside>
+  return (
+    <aside className="col-span-3 flex flex-col gap-2 py-10 ">
+      {siteConfig.sidebar.map((item) => {
+        const isActive = pathname.includes(item.href)
+        console.log(pathname)
+        return (
+          <Link
+            key={item.name}
+            href={item.href}
+            className={`capitalize ${
+              isActive ? "font-semibold" : "text-muted-foreground font-medium"
+            }`}
+          >
+            {item.name}
+          </Link>
+        )
+      })}
+    </aside>
+  )
 }
 
 export default DocsSideBar
