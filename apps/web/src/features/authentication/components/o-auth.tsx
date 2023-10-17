caf0fb84088be14cc50b387f808caf3780902312
xeuxdev <tochukwujohn24@gmail.com>
2023-10-17 15:03:20 +0100

    chore: auth - add login route, oauth signin function

--- apps/web/src/features/authentication/components/o-auth.tsx
+++ apps/web/src/features/authentication/components/o-auth.tsx
@@ -1,10 +1,22 @@
 "use client"
 import { Button } from "@/components/ui/button"
+import { signIn } from "next-auth/react"
 import React from "react"
 
 function OAuthSignIn() {
+  const handleGoogleSignin = async () => {
+    await signIn("google", {
+      callbackUrl: "/dashboard",
+      redirect: false,
+    })
+  }
+
   return (
-    <Button variant={"secondary"} className="w-full">
+    <Button
+      variant={"secondary"}
+      className="w-full"
+      onClick={handleGoogleSignin}
+    >
       Sign up with Google
     </Button>
   )
