e4365aa8e6d13bcb73fb95d479ba721d8f4add40
xeuxdev <tochukwujohn24@gmail.com>
2023-11-29 09:42:31 +0100

    chore - update header on web & docs, update intro on docs

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -4,9 +4,25 @@ import { useRouter } from "next/router"
 
 const config: DocsThemeConfig = {
   logo: (
-    <span className="text-sm font-bold text-primary md:text-xl ">
-      Easy Mailer
-    </span>
+    <div className="flex items-center gap-4">
+      <p className="text-lg font-bold text-primary md:text-xl ">EasyMailer</p>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="text-[#BA1B1B]"
+      >
+        <path d="m22 2-7 20-4-9-9-4Z"></path>
+        <path d="M22 2 11 13"></path>
+      </svg>
+    </div>
   ),
   project: {
     link: "https://github.com/xeuxdev/easymailer",
@@ -19,7 +35,7 @@ const config: DocsThemeConfig = {
       (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
 
     return (
-      <>
+      <html lang="en">
         <title className="font-bold">{`${
           pathname == "/"
             ? "Easymailer"
@@ -27,13 +43,17 @@ const config: DocsThemeConfig = {
               pathname.replaceAll("/", "").slice(1) +
               " - Easymailer"
         }`}</title>
+        <meta
+          name="description"
+          content="Easymailer Docs - Documentation for Easymailer"
+        />
         <meta property="og:url" content={url} />
         <meta property="og:title" content={frontMatter.title || "Easymailer"} />
         <meta
           property="og:description"
           content={frontMatter.description || "Easymailer docs"}
         />
-      </>
+      </html>
     )
   },
   docsRepositoryBase: "https://github.com/xeuxdev/easymailer",
